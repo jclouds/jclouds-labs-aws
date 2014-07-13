c84ccb739f5e30f0305f9d5fda773648b9b21258
Roman Coedo <romancoedo@gmail.com>
2014-07-13 12:22:50 +0200

    JCLOUDS-457: BlobStore Skeleton
    The Amazon Glacier BlobStore skeleton has been added.

---
+++ glacier/src/main/java/org/jclouds/glacier/blobstore/GlacierBlobStore.java
@@ -0,0 +1,126 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jclouds.glacier.blobstore;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Set;
+
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.domain.BlobMetadata;
+import org.jclouds.blobstore.domain.PageSet;
+import org.jclouds.blobstore.domain.StorageMetadata;
+import org.jclouds.blobstore.internal.BaseBlobStore;
+import org.jclouds.blobstore.options.CreateContainerOptions;
+import org.jclouds.blobstore.options.GetOptions;
+import org.jclouds.blobstore.options.ListContainerOptions;
+import org.jclouds.blobstore.options.PutOptions;
+import org.jclouds.blobstore.util.BlobUtils;
+import org.jclouds.collect.Memoized;
+import org.jclouds.crypto.Crypto;
+import org.jclouds.domain.Location;
+import org.jclouds.glacier.GlacierClient;
+import org.jclouds.glacier.domain.ArchiveMetadataCollection;
+import org.jclouds.javax.annotation.Nullable;
+
+import com.google.common.base.Supplier;
+import com.google.common.cache.Cache;
+import com.google.inject.Inject;
+
+public class GlacierBlobStore extends BaseBlobStore {
+   private final GlacierClient sync;
+   private final Crypto crypto;
+   private final Cache<String, ArchiveMetadataCollection> inventoryCache;
+   private final Cache<String, String> archiveCache;
+
+   @Inject
+   GlacierBlobStore(BlobStoreContext context, BlobUtils blobUtils, Supplier<Location> defaultLocation,
+                    @Memoized Supplier<Set<? extends Location>> locations, GlacierClient sync, Crypto crypto,
+                    Cache<String, ArchiveMetadataCollection> inventoryCache, Cache<String, String> archiveCache) {
+      super(context, blobUtils, defaultLocation, locations);
+      this.sync = checkNotNull(sync, "sync");
+      this.crypto = checkNotNull(crypto, "crypto");
+      this.inventoryCache = checkNotNull(inventoryCache, "inventoryCache");
+      this.archiveCache = checkNotNull(archiveCache, "archiveCache");
+   }
+
+   @Override
+   protected boolean deleteAndVerifyContainerGone(String container) {
+      sync.deleteVault(container);
+      return this.containerExists(container);
+   }
+
+   @Override
+   public PageSet<? extends StorageMetadata> list() {
+      return null;
+   }
+
+   @Override
+   public boolean containerExists(String container) {
+      return sync.describeVault(container) != null;
+   }
+
+   @Override
+   public boolean createContainerInLocation(@Nullable Location location, String container) {
+      return sync.createVault(container) != null;
+   }
+
+   @Override
+   public boolean createContainerInLocation(@Nullable Location location, String container,
+                                            CreateContainerOptions options) {
+      return createContainerInLocation(location, container);
+   }
+
+   @Override
+   public PageSet<? extends StorageMetadata> list(String s, ListContainerOptions listContainerOptions) {
+      return null;
+   }
+
+   @Override
+   public boolean blobExists(String s, String s2) {
+      return false;
+   }
+
+   @Override
+   public String putBlob(String container, Blob blob) {
+      return sync.uploadArchive(container, blob.getPayload(), blob.getMetadata().getName());
+   }
+
+   @Override
+   public String putBlob(String container, Blob blob, PutOptions options) {
+      if(options.isMultipart()) {
+         //sync.initiateMultipartUpload(container)
+      }
+      return null;
+   }
+
+   @Override
+   public BlobMetadata blobMetadata(String s, String s2) {
+      return null;
+   }
+
+   @Override
+   public Blob getBlob(String s, String s2, GetOptions getOptions) {
+      return null;
+   }
+
+   @Override
+   public void removeBlob(String s, String s2) {
+
+   }
+}
